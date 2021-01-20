633c43a50f6dfc83e303c6240d1c1be419696511
JachaEras <75362467+JachaEras@users.noreply.github.com>
2021-01-21 00:07:42 +0100

    Html geupdate

--- onepager/functionaliteit.js
+++ onepager/functionaliteit.js
@@ -4,13 +4,14 @@ const oudproductSection = document.querySelector("#oudproductsection")
 oudproductButton.addEventListener("click", function(){
     oudproductSection.scrollIntoView({behaviour:'smooth'});
 })
-
-
-
-
 const verbeteringButton = document.querySelector("#verbetering")
 const verbeteringSection = document.querySelector("#verbeteringsection")
 
 verbeteringButton.addEventListener("click", function(){
     verbeteringSection.scrollIntoView({behaviour:'smooth'});
 })
+
+
+
+
+
